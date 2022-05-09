9990b5d81092a13b1f6f512ccb4d2b662b134a14
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2022-05-09 16:38:49 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Check for dacpac once a minute (in the background).
-while true; do sleep 60; /run/db/dacpac.sh; done &
-
-# Run SQL Server in the foreground.
-/opt/mssql/bin/sqlservr
