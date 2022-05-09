be34d7add113834300daf8e2962ba870e5bb9d76
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2022-05-09 14:10:10 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check for dacpac once a minute (in the background).
-while true; do sleep 60; /run/db/dacpac.sh; done &
+while true; do sleep 60; /bin/sh /run/db/dacpac.sh; done &
 
 # Run SQL Server in the foreground.
 /opt/mssql/bin/sqlservr
